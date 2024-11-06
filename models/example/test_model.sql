826f8ea2717b3133068bd6e4abb7244eb076b102
sainathr357@gmail.com <sainathr357@gmail.com>
2024-11-06 04:33:49 +0000

    msg

--- models/example/test_model.sql
+++ models/example/test_model.sql
@@ -1,10 +1,10 @@
--- SELECT
+/*SELECT
 
--- '100'       AS EMP_ID,
--- 'RAVI'      AS EMP_NAME,
--- '50'        AS AGE,
--- '200000'    AS SALARY,
--- 'HR'        AS DEPARTMENT
+'100'       AS EMP_ID,
+'RAVI'      AS EMP_NAME,
+'50'        AS AGE,
+'200000'    AS SALARY,
+'HR'        AS DEPARTMENT */
 
 
 SELECT
