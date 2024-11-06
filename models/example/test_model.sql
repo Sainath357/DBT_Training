8476ee08c5ddaf072688bb34d3b9b4801e1765ee
sainathr357@gmail.com <sainathr357@gmail.com>
2024-11-06 04:34:56 +0000

    msg

--- models/example/test_model.sql
+++ models/example/test_model.sql
@@ -8,7 +8,7 @@
 
 
 SELECT
-    CAST('100' AS INT(10)) AS EMP_ID,           
+    CAST('100' AS INT) AS EMP_ID,           
     CAST('RAVI' AS VARCHAR(50)) AS EMP_NAME,       
     CAST('50' AS INT) AS AGE,                      
     CAST('200000' AS DECIMAL(10, 2)) AS SALARY, 
