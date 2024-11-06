fe1d2a4ed5c6fa39419ab7439529c4584c6ae325
sainathr357@gmail.com <sainathr357@gmail.com>
2024-11-06 04:32:17 +0000

    msg

--- models/example/test_model.sql
+++ models/example/test_model.sql
@@ -1,15 +1,15 @@
-SELECT
-
-'100'       AS EMP_ID,
-'RAVI'      AS EMP_NAME,
-'50'        AS AGE,
-'200000'    AS SALARY,
-'HR'        AS DEPARTMENT
-
-
 -- SELECT
---     CAST('100' AS INT(10)) AS EMP_ID,           
---     CAST('RAVI' AS VARCHAR(50)) AS EMP_NAME,       
---     CAST('50' AS INT) AS AGE,                      
---     CAST('200000' AS DECIMAL(10, 2)) AS SALARY, 
---     CAST('HR' AS VARCHAR(50)) AS DEPARTMENT        
+
+-- '100'       AS EMP_ID,
+-- 'RAVI'      AS EMP_NAME,
+-- '50'        AS AGE,
+-- '200000'    AS SALARY,
+-- 'HR'        AS DEPARTMENT
+
+
+SELECT
+    CAST('100' AS INT(10)) AS EMP_ID,           
+    CAST('RAVI' AS VARCHAR(50)) AS EMP_NAME,       
+    CAST('50' AS INT) AS AGE,                      
+    CAST('200000' AS DECIMAL(10, 2)) AS SALARY, 
+    CAST('HR' AS VARCHAR(50)) AS DEPARTMENT        
